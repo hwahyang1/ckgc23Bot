3905b87b21b2472988de4ac811359b1e34fd83fa
HwaHyang <me@hwahyang.space>
2022-11-11 16:54:28 +0900

    20221111a
    - 서버에 유저가 참여 시 기본 역할이 부여되지 않던 버그 수정
    - 공지가 전송되게 추가
    - 메시지 갱신 구문에서 기존 메시지 제거 부분을 분리
    - 기존 역할 부여 Embed에 Footer와 Timestamp 추가
    - 파트 역할 부여시에 기본 역할이 제거되지 않던 버그 수정
    - 일부 하드코딩 제거

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 const path = require("path");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 const { Client, GatewayIntentBits, TextChannel, SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
 
 const config = require(path.join(__dirname, '..', 'config', 'config.json'));
@@ -13,24 +13,30 @@ let gameRoleConfig = undefined;
 
 const idRule = /^[a-zA-Z]{1,10}$/;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
 
 /////////////// Functions
 const refreshSlashCommands = async () => {
 	const commands:Array<typeof SlashCommandBuilder> = [
-		new SlashCommandBuilder().setName('add').setDescription('부여받을 수 있는 역할을 추가합니다. (관리자 전용)')
+		new SlashCommandBuilder().setName('notice').setDescription('역할 부여 채널에 공지 메시지를 작성합니다. (봇 관리자 전용)')
+		.addBooleanOption((option) => option.setName('기록제거').setDescription('기존 메시지를 제거 할 지 지정합니다.').setRequired(true))
+		.addStringOption((option) => option.setName('파일명').setDescription('해당되는 공지방에 보낼 파일명을 지정합니다.').setRequired(true)),
+
+		new SlashCommandBuilder().setName('add').setDescription('부여받을 수 있는 역할을 추가합니다. (서버 관리자 전용)')
 			.addRoleOption((option) => option.setName('역할').setDescription('추가할 역할을 지정합니다.').setRequired(true))
 			.addStringOption((option) => option.setName('영문이름').setDescription('역할의 영문 이름을 지정합니다. 공백 없이 대/소문자로만 구성합니다.').setRequired(true)),
-		new SlashCommandBuilder().setName('remove').setDescription('부여받을 수 있는 역할을 제거합니다. (관리자 전용)')
+
+		new SlashCommandBuilder().setName('remove').setDescription('부여받을 수 있는 역할을 제거합니다. (서버 관리자 전용)')
 			.addRoleOption((option) => option.setName('역할').setDescription('제거할 역할을 지정합니다.').setRequired(true)),
-		new SlashCommandBuilder().setName('refresh').setDescription('메시지를 다시 발송합니다. (관리자 전용)'),
+
+		new SlashCommandBuilder().setName('refresh').setDescription('메시지를 다시 발송합니다. (서버 관리자 전용)'),
 	];
 
 	const rest = new REST({ version: '9' }).setToken(config.Token);
     try {
       	await rest.put(
-        	Routes.applicationGuildCommands('1037869521098252388', config.GuildId),
-        	{ body: commands },
+        	Routes.applicationGuildCommands(client.application.id, config.GuildId),
+        	{ body: commands }
       	);
   
       	console.log('Successfully reloaded application (/) commands.');
@@ -39,17 +45,28 @@ const refreshSlashCommands = async () => {
     }
 };
 
-const refreshMessage = async () => {
+const deleteAllMessage = async () => {
 	// 분야별 역할
 	let channel = client.channels.cache.get(partRoleConfig.Notice.ChannelId) as typeof TextChannel;
 	let messageManager = channel.messages;
 	let messages = await messageManager.channel.messages.fetch({ limit: 100 });
 	channel.bulkDelete(messages, true);
 
+	// 게임 역할
+	channel = client.channels.cache.get(gameRoleConfig.Notice.ChannelId) as typeof TextChannel;
+	messageManager = channel.messages;
+	messages = await messageManager.channel.messages.fetch({ limit: 100 });
+	channel.bulkDelete(messages, true);
+}
+
+const refreshMessage = async () => {
+	// 분야별 역할
 	const partRoleMessageEmbed = new EmbedBuilder()
 		.setColor(0xF67720)
 		.setTitle(partRoleConfig.Notice.EmbedTitle)
-		.setDescription(partRoleConfig.Notice.EmbedDescription);
+		.setDescription(partRoleConfig.Notice.EmbedDescription)
+		.setFooter("본 메시지는 상황에 따라 다시 전송 될 수도 있습니다.")
+		.setTimestamp(new Date());
 		
 	const partRoleMessageButtons = new ActionRowBuilder()
 	for (const role of partRoleConfig.Roles) {
@@ -66,7 +83,9 @@ const refreshMessage = async () => {
 	const partRoleDividerMessageEmbed = new EmbedBuilder()
 		.setColor(0xF67720)
 		.setTitle('필수 역할')
-		.setDescription('아래의 역할(버튼)은 필수로 눌러서 가져가세요.');
+		.setDescription('아래의 역할(버튼)은 필수로 눌러서 가져가세요.')
+		.setFooter("본 메시지는 상황에 따라 다시 전송 될 수도 있습니다.")
+		.setTimestamp(new Date());
 		
 	const partRoleDividerMessageButtons = new ActionRowBuilder()
 	.addComponents(
@@ -76,17 +95,14 @@ const refreshMessage = async () => {
 			.setStyle(ButtonStyle.Primary)
 	);
 	await (client.channels.cache.get(partRoleConfig.Notice.ChannelId) as typeof TextChannel).send({ embeds: [partRoleDividerMessageEmbed], components: [partRoleDividerMessageButtons] });
-	
-	// 게임 역할
-	channel = client.channels.cache.get(gameRoleConfig.Notice.ChannelId) as typeof TextChannel;
-	messageManager = channel.messages;
-	messages = await messageManager.channel.messages.fetch({ limit: 100 });
-	channel.bulkDelete(messages, true);
 
+	// 게임 역할
 	const gameRoleMessageEmbed = new EmbedBuilder()
 		.setColor(0xF67720)
 		.setTitle(gameRoleConfig.Notice.EmbedTitle)
-		.setDescription(gameRoleConfig.Notice.EmbedDescription);
+		.setDescription(gameRoleConfig.Notice.EmbedDescription)
+		.setFooter("본 메시지는 상황에 따라 다시 전송 될 수도 있습니다.")
+		.setTimestamp(new Date());
 		
 	let gameRoleMessageButtons = undefined;
 	let i = 0;
@@ -114,8 +130,9 @@ const refreshMessage = async () => {
 client.on('ready', async () => {
   	console.log(`Logged in as ${client.user.tag}!`);
 
-	refreshSlashCommands();
-	//refreshMessage();
+	await refreshSlashCommands();
+	//await deleteAllMessage();
+	//await refreshMessage();
 });
 
 client.on('guildMemberAdd', async member => {
@@ -127,11 +144,50 @@ client.on('guildMemberAdd', async member => {
 client.on('interactionCreate', async interaction => {
 	if (interaction.isCommand()) {
 		if (interaction.guild.id !== config.GuildId) return;
+		if (interaction.commandName === 'notice') {
+			if (interaction.member.id !== config.BotOwner) {
+				await interaction.reply({ content: '요청을 처리하지 못했습니다.\n`권한이 없습니다: Bot Owner(config/config.json)`', ephemeral: true });
+				return;
+			}
+
+			const messageFileName: string = interaction.options.getString('파일명');
+			const deleteMessage: boolean = interaction.options.getBoolean('기록제거');
+
+			if (!await fs.existsSync(`noticeData/${messageFileName}`)) {
+				await interaction.reply({ content: `요청을 처리하지 못했습니다.\n\`해당 경로에 파일이 존재하지 않습니다: ${messageFileName}\``, ephemeral: true });
+				return;
+			}
+
+			let rawdata = await fs.readFileSync(`noticeData/${messageFileName}`);
+			let messageData = JSON.parse(rawdata);
+
+			const messageEmbed = new EmbedBuilder().setColor(0xF67720);
+			if (messageData.Title === null)	messageEmbed.setTitle(messageData.Title);
+			if (messageData.Description === null) messageEmbed.setDescription(messageData.Description);
+			if (messageData.Footer === null) messageEmbed.setFooter(messageData.Footer);
+			if (messageData.Timestamp) messageEmbed.setTimestamp(new Date());
+			for (const field of messageData) {
+				messageEmbed.addFields({
+					name: field.Title,
+					value: field.Description
+				});
+			};
+
+			if (deleteMessage) {
+				await deleteAllMessage();
+			}
+
+			await (client.channels.cache.get(partRoleConfig.Notice.ChannelId) as typeof TextChannel).send({ embeds: [messageEmbed] });
+			await (client.channels.cache.get(gameRoleConfig.Notice.ChannelId) as typeof TextChannel).send({ embeds: [messageEmbed] });
+
+			await interaction.reply({ content: '해당 채널에 메시지를 전송했습니다.', ephemeral: true });
+		}
 		if (interaction.commandName === 'refresh') {
 			if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
 				await interaction.reply({ content: '요청을 처리하지 못했습니다.\n`권한이 없습니다: Administrator(0x8)`', ephemeral: true });
 				return;
 			}
+			await deleteAllMessage();
 			await refreshMessage();
 			await interaction.reply({ content: '해당 채널에 메시지를 전송했습니다.', ephemeral: true });
 		}
@@ -142,7 +198,7 @@ client.on('interactionCreate', async interaction => {
 			}
 			
 			const role = interaction.options.getRole('역할');
-			const id = interaction.options.getString('영문이름');
+			const id: string = interaction.options.getString('영문이름');
 
 			if (!idRule.test(id)) {
 				await interaction.reply({ content: '요청을 처리하지 못했습니다.\n`영문 이름이 규격에 맞지 않습니다: 영어 대/소문자, 1~10자 이내`', ephemeral: true });
@@ -206,7 +262,7 @@ client.on('interactionCreate', async interaction => {
 
 			try {
 				for (const role of partRoleConfig.Roles) {
-					let rData = interaction.member.roles.cache.find(target => target.id === role.roleId);
+					let rData = interaction.member.roles.cache.find(target => target.id === role.roleId || target.id === role.DefaultRoleId);
 					if (rData !== undefined) {
 						await interaction.member.roles.remove(rData);
 					}
